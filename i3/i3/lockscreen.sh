ccbdedf9c915b948d4742ee3fc7c94b89b307699
Mrinaal Dogra <mrinaald@iitk.ac.in>
2021-01-12 10:34:30 +0530

    [i3] Update config files to support xfce4 packages.

--- i3/i3/lockscreen.sh
+++ i3/i3/lockscreen.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
-gnome-screenshot -f /tmp/lockscreen.png 2> /dev/null
+imgfile="/home/mrinaal/Pictures/Wallpapers/the-amazing-spider-man-spider-uhd-4k-lockscreen.png"
 
-convert -blur 10x10 /tmp/lockscreen.png /tmp/lockscreen.png
-
-i3lock -i /tmp/lockscreen.png
+if [[ -f $imgfile ]]; then
+    i3lock -i $imgfile
+else
+    # gnome-screenshot -f /tmp/lockscreen.png 2> /dev/null
+    xfce4-screenshooter -fs /tmp/lockscreen.png 2> /dev/null
+    convert -blur 10x10 /tmp/lockscreen.png /tmp/lockscreen.png
+    i3lock -i /tmp/lockscreen.png
+fi
